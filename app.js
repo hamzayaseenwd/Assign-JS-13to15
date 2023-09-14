accd23c148bb6b9518e6f993727297b8f5b0904e
hamzayaseenwd <hamzayaseen836@gmail.com>
2023-09-14 20:51:23 +0500

    edition

--- app.js
+++ app.js
@@ -1,27 +1,23 @@
 // Q1
 
+var arr= []
 
+console.log(arr)
 
+// Adding student names to the array
+arr.push("");
 
-
-
-
-
-
-
-
-
-
+// Accessing the array elements
+console.log(arr[0]);
 
 
 
 // Q2
 
 
+var arr= [];
 
-
-
-
+console.log(arr)
 
 
 
@@ -29,79 +25,48 @@
 // Q3
 
 
+var Str = ["string"]
 
 
-
-
-
-
-
-
+console.log( )
 
 
 
 // Q4
 
 
+var arr = [123]
 
 
-
-
-
-
-
-
+console.log(arr)
 
 
 
 // Q5
 
 
+var arr = [true]
+var arr = [false]
 
 
-
-
-
-
-
-
-
+console.log(arr)
 
 
 // Q6
 
+var arr = ["Hamza", false, 123, true] 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
+console.log(arr)
 
 
 
 // Q7
 
+var Qualifications = ["QUALIFICATIONS </br>, SSC,</br>, HSC,</br>, BCS,</br>, BS,</br>, BCOM,</br>, MS,</br>, M. Phil.,</br>, PhD "] 
 
 
-
-
-
-
-
-
-
-
-
-
-
+document.write(Qualifications)
 
 
 
@@ -111,17 +76,23 @@
 
 
 
+var arrName = ["Faraz", "Anas", "Hamza"];
+var studentMarks = [320,230,480]
 
+var percentage = studentMarks[0]  / 500*100;
+var percentage1 = studentMarks[1]  / 500*100;
+var percentage2 = studentMarks[2]  / 500*100;
 
+console.log(percentage)
+var score1 = "Score of " + " "+ arrName[0]+ " "+ "is" + " "+ studentMarks[0]+ ". "+ "Percentage is "+ " "+ percentage +"%"
 
+console.log(percentage1)
+var score2 = "Score of " + " "+ arrName[1]+ " "+ "is" + " "+ studentMarks[1]+ ". "+ "Percentage is "+ " "+ percentage1 +"%"
 
+console.log(percentage2)
+var score3 = "Score of " + " "+ arrName[2]+ " "+ "is" + " "+ studentMarks[2]+ ". "+ "Percentage is "+ " "+ percentage2 +"%"
 
-
-
-
-
-
-
+document.write("</br>",score1, "</br>", score2, "</br>", score3, "</br>") 
 
 
 
@@ -148,15 +119,13 @@
 
 
 
+var std = [320,230,480,120,"<br/>" ];
 
+// std.sort (also work)
+var aorder = std.sort()
 
-
-
-
-
-
-
-
+console.log(aorder)
+document.write(aorder)
 
 
 
@@ -167,7 +136,12 @@
 
 
 
+var cityNames = ["</br>","Karachi", "Laore", "Islamabad", "Multan", "Pishawar"];
 
+var name = cityNames.slice (1,4);
+
+console.log(name)
+document.write(name)
 
 
 
